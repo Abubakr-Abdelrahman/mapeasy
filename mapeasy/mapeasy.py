ae4151946ed484acf9080316ace8e5e73f82e8f4
Abubakr Abdelrahman <bakri_abdo@yahoo.com>
2023-11-03 04:31:27 +0400

    gg

--- mapeasy/mapeasy.py
+++ mapeasy/mapeasy.py
@@ -6,7 +6,7 @@ import ipyleaflet
 
 class Map(ipyleaflet.Map):
 
-    def __init__(self, center, zoom, **kwargs) -> None:
+    def __init__(self, center=[20, 0], zoom=2, **kwargs) -> None:
 
         if "scroll_wheel_zoom" not in kwargs:
             kwargs["scroll_wheel_zoom"] = True
